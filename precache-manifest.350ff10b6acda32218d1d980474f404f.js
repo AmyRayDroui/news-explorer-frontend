7d11177032309abdb9617a1ef5aef9e1b2a36aef
AmyRayDroui <amyraydroui@gmail.com>
2023-03-07 23:40:04 +0200

    Updates

---
+++ precache-manifest.350ff10b6acda32218d1d980474f404f.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "48be7bbf746b29194c20",
+    "url": "/news-explorer-frontend/static/css/main.29490f95.chunk.css"
+  },
+  {
+    "revision": "48be7bbf746b29194c20",
+    "url": "/news-explorer-frontend/static/js/main.48be7bbf.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/news-explorer-frontend/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "680cc138b88fe2664dcb",
+    "url": "/news-explorer-frontend/static/js/2.680cc138.chunk.js"
+  },
+  {
+    "revision": "39dd03efb013dfb90a0a",
+    "url": "/news-explorer-frontend/static/js/runtime~main.39dd03ef.js"
+  },
+  {
+    "revision": "c1e53c08151f7b6ddcc431450366d340",
+    "url": "/news-explorer-frontend/static/media/lakeView.c1e53c08.jpg"
+  },
+  {
+    "revision": "d20e1b6de370157dbe65517098b82c10",
+    "url": "/news-explorer-frontend/static/media/moose.d20e1b6d.jpg"
+  },
+  {
+    "revision": "5c8d476eb3ffb48ed8e606d31439c2c3",
+    "url": "/news-explorer-frontend/static/media/dogo.5c8d476e.jpg"
+  },
+  {
+    "revision": "ec7c97bb93a3b2587858c28613d62c3e",
+    "url": "/news-explorer-frontend/static/media/about-author.ec7c97bb.jpg"
+  },
+  {
+    "revision": "456cb59d2cc9fa18f7b48cc91e4c8337",
+    "url": "/news-explorer-frontend/static/media/nightSky.456cb59d.jpg"
+  },
+  {
+    "revision": "4d6b2639f12384096608511ab2d4d025",
+    "url": "/news-explorer-frontend/static/media/hotspring.4d6b2639.jpg"
+  },
+  {
+    "revision": "5eaa822fa94d8d6ba46cd49e43410e32",
+    "url": "/news-explorer-frontend/static/media/github-icon.5eaa822f.svg"
+  },
+  {
+    "revision": "afdf6e40e81573502a57f8242cdd35d2",
+    "url": "/news-explorer-frontend/static/media/facebook-icon.afdf6e40.svg"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/news-explorer-frontend/static/media/roboto-400-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/news-explorer-frontend/static/media/roboto-500.87284894.woff"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/news-explorer-frontend/static/media/roboto-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/news-explorer-frontend/static/media/roboto-500.020c97dc.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/news-explorer-frontend/static/media/roboto-700.adcde98f.woff"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/news-explorer-frontend/static/media/roboto-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/news-explorer-frontend/static/media/roboto-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "ae3bdea1a1337e878e00c40d5be1e8a4",
+    "url": "/news-explorer-frontend/static/media/inter-400-regular.ae3bdea1.woff2"
+  },
+  {
+    "revision": "41e86578ed11f2de28bc5e90b5b322f2",
+    "url": "/news-explorer-frontend/static/media/inter-400-regular.41e86578.woff"
+  },
+  {
+    "revision": "6e76a4cee4838c4ffac07e8b2be5f187",
+    "url": "/news-explorer-frontend/static/media/inter-500.6e76a4ce.woff"
+  },
+  {
+    "revision": "eb89003999cea37e721a490d2d056e8d",
+    "url": "/news-explorer-frontend/static/media/inter-500.eb890039.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/news-explorer-frontend/static/media/roboto-400-regular.479970ff.woff2"
+  },
+  {
+    "revision": "07ebe96f8fd53717e959ca356e69234c",
+    "url": "/news-explorer-frontend/static/media/roboto-slab-400-regular.07ebe96f.woff2"
+  },
+  {
+    "revision": "3125381951c8362bf2515eec9f957655",
+    "url": "/news-explorer-frontend/static/media/SourceSansPro-Regular.31253819.woff2"
+  },
+  {
+    "revision": "e2c2aa3f2d32159a3270d8c1d7c9d015",
+    "url": "/news-explorer-frontend/static/media/SourceSansPro-Regular.e2c2aa3f.woff"
+  },
+  {
+    "revision": "28fdd9b8091dc170ba414b656f2bac8c",
+    "url": "/news-explorer-frontend/static/media/roboto-slab-400-regular.28fdd9b8.woff"
+  },
+  {
+    "revision": "ed5c465aad8ef4caa033bf747f72b3d2",
+    "url": "/news-explorer-frontend/static/media/roboto-slab-700.ed5c465a.woff2"
+  },
+  {
+    "revision": "607d7c73a1de7fd35204ee35d32fd8f9",
+    "url": "/news-explorer-frontend/static/media/roboto-slab-700.607d7c73.woff"
+  },
+  {
+    "revision": "cc9bdc2ae5d69f2faea7224b8e1faa1e",
+    "url": "/news-explorer-frontend/static/media/not-found-icon.cc9bdc2a.svg"
+  },
+  {
+    "revision": "5efd35d2e1822499a70f852c0795e32e",
+    "url": "/news-explorer-frontend/static/media/grey-bookmark-icon.5efd35d2.svg"
+  },
+  {
+    "revision": "240acee0fd06c8e68582dcf9353b8412",
+    "url": "/news-explorer-frontend/static/media/black-bookmark-icon.240acee0.svg"
+  },
+  {
+    "revision": "f92b7c1ae1c4784cd537f077b74fc6f7",
+    "url": "/news-explorer-frontend/static/media/marked-bookmark-icon.f92b7c1a.svg"
+  },
+  {
+    "revision": "33bb9dfdcb7ffc7d74b6c8f77129381f",
+    "url": "/news-explorer-frontend/static/media/grey-bin-icon.33bb9dfd.svg"
+  },
+  {
+    "revision": "85bdc9b11a1dda5a1675fd4b3df763f7",
+    "url": "/news-explorer-frontend/static/media/black-bin-icon.85bdc9b1.svg"
+  },
+  {
+    "revision": "835c0a2294124034bc806a8a155f89a7",
+    "url": "/news-explorer-frontend/static/media/search-form_background.835c0a22.jpg"
+  },
+  {
+    "revision": "ca037225afbac4c8e331b6a4fb0d3ab5",
+    "url": "/news-explorer-frontend/static/media/close_icon.ca037225.svg"
+  },
+  {
+    "revision": "24c085fac85e2aa196e87d5aab33cfa2",
+    "url": "/news-explorer-frontend/static/media/menu-black-icon.24c085fa.svg"
+  },
+  {
+    "revision": "c366a189b6626cf62c1e024c8fb8c876",
+    "url": "/news-explorer-frontend/static/media/menu-white-icon.c366a189.svg"
+  },
+  {
+    "revision": "aee1bcdfd3cdc9f7df65571b1633cebd",
+    "url": "/news-explorer-frontend/static/media/exit-white-icon.aee1bcdf.svg"
+  },
+  {
+    "revision": "e8a3e37803382fed5205286b47f64362",
+    "url": "/news-explorer-frontend/static/media/exit-black-icon.e8a3e378.svg"
+  },
+  {
+    "revision": "6fe104a6bac07603adf170b893c5cea8",
+    "url": "/news-explorer-frontend/index.html"
+  }
+];
